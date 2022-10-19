a34e052de86e5c52c3ffdc44b908d97213aaeb4c
Steven Anderson <sande139@d127.org>
2022-10-19 09:29:28 -0500

    Initial Commit

---
+++ Display a number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Display a number
+//
+//  Created by Steven M Anderson (Student) on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
